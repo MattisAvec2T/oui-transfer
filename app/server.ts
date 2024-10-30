f0be7e10bfe4a835bc14116bed95a8835697539d
Nyoote <faustinecharrier@gmail.com>
2024-10-30 11:39:46 +0100

    feat: add connexion to database

--- app/server.ts
+++ app/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import pool from 'db'
 
 const server = express()
 const port = 3000
@@ -8,5 +9,17 @@ server.get("/", (req, res, next) => {
         message: "Test"
     })
 })
+
+
+server.get('/test-db', async (req, res) => {
+    try {
+        const rows = await pool.query('SELECT * from users');
+        res.json({ message: 'Connexion à la base de données réussie!', rows });
+    } catch (error) {
+        console.error('Database connection error:', error);
+        res.status(500).json({ error: 'Erreur de connexion à la base de données', details: error.message });
+    }
+    
+});
 
 server.listen(port, () => console.log(`App running on port ${port}`))
