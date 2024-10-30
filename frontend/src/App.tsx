095e374c33659ffa5461cb18441679dc04be9ed7
Nyoote <faustinecharrier@gmail.com>
2024-10-30 16:50:33 +0100

    feat: missing front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,24 +5,24 @@ import Dashboard from './components/Dashboard';
 import './App.css';
 
 const App: React.FC = () => {
-    const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
+  const [uploadedFiles, setUploadedFiles] = useState<{ name: string; alias: string }[]>([]);
 
-    const handleFileUpload = (file: File) => {
-        setUploadedFiles((prevFiles) => [...prevFiles, file]);
-    };
+  const handleFileUpload = (file: File, alias: string) => {
+    setUploadedFiles((prevFiles) => [...prevFiles, { name: file.name, alias }]);
+  };
 
-    return (
-        <div>
-            <h1>Oui-Transfer</h1>
-            <nav>
-                <Link to="/">Uploader un Fichier</Link> | <Link to="/dashboard">Tableau de Bord</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<FileUpload onFileUpload={handleFileUpload} />} />
-                <Route path="/dashboard" element={<Dashboard uploadedFiles={uploadedFiles} />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Oui-Transfer</h1>
+      <nav>
+        <Link to="/">Uploader un Fichier</Link> | <Link to="/dashboard">Tableau de Bord</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<FileUpload onFileUpload={handleFileUpload} />} />
+        <Route path="/dashboard" element={<Dashboard uploadedFiles={uploadedFiles} />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
